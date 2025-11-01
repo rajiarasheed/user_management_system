f35a5828263f4a9a7fff3af35619f48a2510e973
Rajia Kolleni <rajiaabdulrasheed@gmail.com>
2025-11-01 15:34:52 +0530

    deleted old datas in db, add password security-encryption with hash

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require("../models/userModel");
 
+const bcrypt = require('bcrypt');
+const securePassword=async(password)=>{
+    try {
+        const passwordHash=await bcrypt.hash(password,10);
+        return passwordHash;
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+}
+
 const loadRegister= async(req,res)=>{
     try {
         res.render("registration")
@@ -10,12 +21,13 @@ const loadRegister= async(req,res)=>{
 
 const insertUser= async(req,res)=>{
     try{
+        const spassword=await securePassword(req.body.password)
         const user= new User({
             name:req.body.name,
             email:req.body.email,
             mobile:req.body.mobile,
             image:req.file.filename,
-            password:req.body.password,
+            password:spassword,
             is_admin:0,
         });
         const userData= await user.save();
