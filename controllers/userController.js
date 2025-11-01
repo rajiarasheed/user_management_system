ac8a73f998800b37ac64424866c32452c0d4f4c5
Rajia Kolleni <rajiaabdulrasheed@gmail.com>
2025-11-01 15:18:12 +0530

    Added registration form submission to database and display success message upon successful registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,30 @@ const loadRegister= async(req,res)=>{
         console.log(error.message);
     }
 }
+
+const insertUser= async(req,res)=>{
+    try{
+        const user= new User({
+            name:req.body.name,
+            email:req.body.email,
+            mobile:req.body.mobile,
+            image:req.file.filename,
+            password:req.body.password,
+            is_admin:0,
+        });
+        const userData= await user.save();
+
+        if(userData){
+            res.render('registration',{message:"Your Registration has been successfull, Please verify your Email..."})
+        }else{
+            res.render('registration',{message:"Your Registration has been successfull..."})
+        }
+    }catch(error){
+        console.log(error.message);
+        
+    }
+}
 module.exports={
-    loadRegister
+    loadRegister,
+    insertUser
 }
